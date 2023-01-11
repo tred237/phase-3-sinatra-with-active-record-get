48a9bf1e07b45ba303a29e1b95085fb011ce9c9c
trevor dewindt <trevor.dewindt@gmail.com>
2023-01-10 23:14:40 -0700

    commit tree 5ac20a15c2e24fdd340aeccb3273a7a4bf8c8ffa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    # get all the games from the database
+    # return a JSON response with an array of all the game data
+    Game.all.to_json
   end
 
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    # send a JSON-formatted response of the game data
+    Game.find(params[:id]).to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
 end
